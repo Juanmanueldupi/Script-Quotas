285400b136933cb96cc8c8ba88a95b826f1d118a
oscarsanabria80 <oscar.poncedeleonsanabria80@gmail.com>
2022-04-04 14:19:54 +0200

    plantilla

---
+++ src/f_plantilla_cuota.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+function f_plantilla_cuota {
+#Comprueba si eres root
+if ! [ $(id -u) = 0 ]; then
+	echo "No eres root"
+	exit
+	else
+	echo "Eres root"
+fi
+#Preguntar  el nombre del usuario y si queremos crearlo.
+read -p  "Nombre del usuario que deseas crear: " USER
+read -p  "Contraseña del usuario:  "  CON
+read -p "¿Deseas continuar con la operacón? S/N: " RESPUESTA
+if [[ $RESPUESTA  == S ]] || [[ $RESPUESTA == s ]]; then
+	echo "En curso..."
+	sleep 2s
+	else
+	exit
+fi
+# Comprueba el usuario y si no esta en el sistema lo crea.
+if id -u $USER 2> /dev/null; then
+	echo " El usuario indicado existe en el sistema "
+	else
+	echo "Creando usuario..."
+	useradd $USER -p $CON
+	echo "¡Usuario creado con exito!"
+fi
+}
+
+f_plantilla_cuota
