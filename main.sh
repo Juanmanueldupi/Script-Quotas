f295e01ca03ba2e2ea7fbb54da7181dd88e71cd1
Evanticks <pl2011_50@hotmail.com>
2022-04-07 13:34:43 +0200

    modificaciones

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
-source src f_compruebaroot.sh
-source src f_UUID.sh
-source src f_configquota.sh
-source src f_existedirectorio.sh
-source src f_habilita_quota.sh
-source src f_plantilla_cuota.sh
+source src/f_compruebaroot.sh
+source src/f_UUID.sh
+source src/f_configquota.sh
+source src/f_existedirectorio.sh
+source src/f_habilita_quota.sh
+source src/f_plantilla_cuota.sh
 
 
 
